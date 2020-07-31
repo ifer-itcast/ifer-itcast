5dfef89137314a65541352e89b18f9e82bc3ded4
ifer-itcast <yangkangkang@itcast.cn>
2020-07-31 20:09:30 +0800

    Site updated: 2020-07-31 20:09:10

---
+++ resource/files/data-handling/data.js
@@ -0,0 +1,213 @@
+let datas = [{
+        name: '家用电器',
+        id: 1,
+        children: [{
+                name: '电视',
+                id: 2,
+                children: [{
+                        name: '超薄电视',
+                        id: 3
+                    },
+                    {
+                        name: '全面屏电视',
+                        id: 4
+                    },
+                    {
+                        name: '智能电视',
+                        id: 5
+                    },
+                    {
+                        name: '4K超清电视',
+                        id: 6
+                    }
+                ]
+            },
+            {
+                name: '空调',
+                id: 7,
+                children: [{
+                        name: '空调挂机',
+                        id: 8
+                    },
+                    {
+                        name: '空调柜机',
+                        id: 9
+                    },
+                    {
+                        name: '中央空调',
+                        id: 10
+                    },
+                    {
+                        name: '变频空调',
+                        id: 11
+                    },
+                    {
+                        name: '移动空调',
+                        id: 12
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        name: '手机数码',
+        id: 13,
+        children: [{
+                name: '手机通讯',
+                id: 14,
+                children: [{
+                        name: '游戏手机',
+                        id: 15
+                    },
+                    {
+                        name: '5G手机',
+                        id: 16
+                    },
+                    {
+                        name: '拍照手机',
+                        id: 17
+                    },
+                    {
+                        name: '全面屏手机',
+                        id: 18
+                    }
+                ]
+            },
+            {
+                name: '手机配件',
+                id: 19,
+                children: [{
+                        name: '手机壳',
+                        id: 20
+                    },
+                    {
+                        name: '贴膜手机',
+                        id: 21
+                    },
+                    {
+                        name: '存储卡',
+                        id: 22
+                    },
+                    {
+                        name: '数据线',
+                        id: 23
+                    },
+                    {
+                        name: '充电器',
+                        id: 24
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        name: '电脑办公',
+        id: 25,
+        children: [{
+                name: '电脑整机',
+                id: 26,
+                children: [{
+                        name: '笔记本',
+                        id: 27,
+                        children: [{
+                                name: 'Lenove',
+                                id: 28
+                            },
+                            {
+                                name: 'HUAWEI',
+                                id: 29
+                            },
+                            {
+                                name: 'ThinkPad',
+                                id: 30
+                            },
+                            {
+                                name: 'Dell',
+                                id: 31
+                            },
+                            {
+                                name: 'HP',
+                                id: 32
+                            },
+                            {
+                                name: 'MI',
+                                id: 33
+                            }
+                        ]
+                    },
+                    {
+                        name: '游戏本',
+                        id: 34
+                    },
+                    {
+                        name: '平板电脑',
+                        id: 35
+                    },
+                    {
+                        name: '台式机',
+                        id: 36
+                    },
+                    {
+                        name: '一体机服务器',
+                        id: 37
+                    }
+                ]
+            },
+            {
+                name: '电脑配件',
+                id: 38,
+                children: [{
+                        name: '显示器',
+                        id: 39
+                    },
+                    {
+                        name: 'CPU',
+                        id: 40
+                    },
+                    {
+                        name: '主板',
+                        id: 41
+                    },
+                    {
+                        name: '显卡',
+                        id: 42
+                    },
+                    {
+                        name: '硬盘',
+                        id: 43
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        name: '新鲜水果',
+        id: 44,
+        children: [{
+                name: '苹果',
+                id: 45
+            },
+            {
+                name: '橙子',
+                id: 46
+            },
+            {
+                name: '奇异果',
+                id: 47
+            },
+            {
+                name: '火龙果',
+                id: 48
+            },
+            {
+                name: '榴莲',
+                id: 49
+            }
+        ]
+    },
+    {
+        name: 'xxx',
+        id: 50,
+        children: []
+    }
+];
